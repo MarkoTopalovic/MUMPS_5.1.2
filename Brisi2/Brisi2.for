7b34e75b42be0ad535300962a6ec380fe5708b17
“Marko <topalovic1981@gmail.com>
2019-01-27 21:24:30 +0100

    brisi2

--- Brisi2/Brisi2.for
+++ Brisi2/Brisi2.for
@@ -12,14 +12,20 @@
 !
 !****************************************************************************
 
-    program Brisi2
+      program Brisi2
 
-    implicit none
-
+      !implicit none
+      IMPLICIT NONE
+      INCLUDE 'mpif.h'
+      INCLUDE 'dmumps_struc.h'
+      TYPE (DMUMPS_STRUC) mumps_par
+      INTEGER IERR, I
+      INTEGER(8) I8
+      CALL MPI_INIT(IERR)
     ! Variables
-     integer i
+      !integer i
     ! Body of Brisi2
-    print *, 'Hello mumps'
-    read(*,*) i
-    end program Brisi2
+      print *, 'Hello mumps'
+      read(*,*) i
+      end program Brisi2
 
