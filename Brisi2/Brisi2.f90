7446734082d65aba28ce6115f0a9ce9eb333bdff
“Marko <topalovic1981@gmail.com>
2019-01-27 11:05:32 +0100

    brisi2

---
+++ Brisi2/Brisi2.f90
@@ -0,0 +1,25 @@
+!  Brisi2.f90 
+!
+!  FUNCTIONS:
+!  Brisi2 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Brisi2
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Brisi2
+
+    implicit none
+
+    ! Variables
+     integer i
+    ! Body of Brisi2
+    print *, 'Hello mumps'
+    read(*,*) i
+    end program Brisi2
+
